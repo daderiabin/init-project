fe1031709e824d1dd62dd0a500f140d7a6ae22b8
daderiabin <danylo.deriabin@gmail.com>
2019-10-26 19:52:03 -0500

    Implemented the leagues show action and view

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class LeaguesController < ApplicationController
   def index
     @leagues = League.all
   end
 
   def show
-    
+    @league = League.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+CardType.destroy_all
+Nation.destroy_all
+League.destroy_all
+Club.destroy_all
+
+puts "Generated #{CardType.count} card types."
+puts "Generated #{Nation.count} nations."
+puts "Generated #{League.count} leagues."
+puts "Generated #{Club.count} clubs."
