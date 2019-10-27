3ee2a3836d9b9ba21ebf920f092b75f5d572a5ff
daderiabin <danylo.deriabin@gmail.com>
2019-10-27 00:03:18 -0500

    Implemented show action and view for nations

--- app/controllers/nations_controller.rb
+++ app/controllers/nations_controller.rb
@@ -1,7 +1,9 @@
 class NationsController < ApplicationController
   def index
+    @nations = Nation.all
   end
 
   def show
+    @nation = Nation.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'nations/index'
-  get 'nations/show'
+  # get 'nations/index'
+  # get 'nations/show'
   resources :leagues, only: %i[index show]
+  resources :nations, only: %i[index show]
 
   # get '/leagues', to: 'leagues#index'
   # get '/leagues/:id', to: 'leagues#show', id: /\d/
