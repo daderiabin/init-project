f6e70a1f46408fc7d371dbdfc2cbfa8d18ef7e9e
daderiabin <danylo.deriabin@gmail.com>
2019-10-29 09:19:13 -0500

    Replaced wrong Page scaffold with a right one.

--- db/migrate/20191029141740_create_pages.rb
+++ db/migrate/20191029141740_create_pages.rb
@@ -2,7 +2,7 @@ class CreatePages < ActiveRecord::Migration[6.0]
   def change
     create_table :pages do |t|
       t.string :title
-      t.string :content
+      t.text :content
       t.string :permalink
 
       t.timestamps
