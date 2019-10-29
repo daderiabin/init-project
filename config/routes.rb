119b993e12776faa326ac99003a4e9faacda40d8
daderiabin <danylo.deriabin@gmail.com>
2019-10-29 09:11:45 -0500

    Added a scaffold for a Page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :pages
   resources :nations, only: %i[index show]
   resources :card_types, only: %i[index show]
   resources :leagues, only: %i[index show]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_185744) do
+ActiveRecord::Schema.define(version: 2019_10_29_141012) do
 
   create_table "card_types", force: :cascade do |t|
     t.string "quality"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2019_10_28_185744) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "img"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "permalink"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "players", force: :cascade do |t|
